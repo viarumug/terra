38c8ad11770b6a6246b3fec23cc8d94a8a6be78c
viarumug <92714071+viarumug@users.noreply.github.com>
2024-01-02 10:55:27 +0530

    Update rds_instance.tf

--- terraform_19_task/rds_instance.tf
+++ terraform_19_task/rds_instance.tf
@@ -5,8 +5,8 @@ resource "aws_db_instance" "hadiya" {
    engine              = "mysql"
    engine_version      = "8.0.33"
    instance_class      = "db.t2.micro"
-   username            = "admin"
-   password            = "Passw0rd!123"
+   username            = "*****"
+   password            = "******"
    parameter_group_name = aws_db_parameter_group.paragroup.name
    vpc_security_group_ids = [aws_security_group.private-sg.id]
    db_subnet_group_name = aws_db_subnet_group.rds_db_subnet.name
